a1074dacba8936b64451b725573fe21722f1397d
Davis782 <67481353+Davis782@users.noreply.github.com>
2021-08-10 17:59:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -64,67 +64,68 @@ if Options == "Currency_Pairs":
 
 #=============================The Beginning=========================================================================================
 
-@st.cache
-def load_data():
-    INCREMENTS=[{
-            '1m': 1,
-            '2m': 2,
-            '5m': 5,
-            '15m': 15,
-            '30m': 3,
-            '60m': 60,
-            '90m': 90,
-            '1h': 1,
-            '1d': 1,
-            #    '5d ': 5,
-            #    '1wk': 1,
-            #    '1mo': 1,
-            #    '3mo': 3
-            }]
+    @st.cache
+    def load_data():
+        INCREMENTS=[{
+                '1m': 1,
+                '2m': 2,
+                '5m': 5,
+                '15m': 15,
+                '30m': 3,
+                '60m': 60,
+                '90m': 90,
+                '1h': 1,
+                '1d': 1,
+                #    '5d ': 5,
+                #    '1wk': 1,
+                #    '1mo': 1,
+                #    '3mo': 3
+                }]
 
-    CURRENCY = ["GBP", "JPY", "CAD", "USD", "EUR", "CHF", "NZD"]
+        CURRENCY = ["GBP", "JPY", "CAD", "USD", "EUR", "CHF", "NZD"]
 
-    start_date = datetime(2021, 7, 27)
-    end_date = datetime(2021, 8, 3)
+        start_date = datetime(2021, 7, 27)
+        end_date = datetime(2021, 8, 3)
 
-    N = 1000000000
+        N = 1000000000
 
-    list_of_pairs = [(CURRENCY[p1] + "" + CURRENCY[p2] + "=X") for p1 in range(len(CURRENCY)) for p2 in
-                     range(p1 + 1, len(CURRENCY))]
+        list_of_pairs = [(CURRENCY[p1] + "" + CURRENCY[p2] + "=X") for p1 in range(len(CURRENCY)) for p2 in
+                         range(p1 + 1, len(CURRENCY))]
 
-    list_of_pairs = pd.DataFrame(list_of_pairs)
-    pairs = list_of_pairs.replace('"', '')
-    pairs = pd.DataFrame(pairs)
-    pairs = pairs.rename(columns={0: "Names1"})
-    pairs = pairs["Names1"]
+        list_of_pairs = pd.DataFrame(list_of_pairs)
+        pairs = list_of_pairs.replace('"', '')
+        pairs = pd.DataFrame(pairs)
+        pairs = pairs.rename(columns={0: "Names1"})
+        pairs = pairs["Names1"]
 
-    keys = pd.DataFrame.from_dict(INCREMENTS).keys()
+        keys = pd.DataFrame.from_dict(INCREMENTS).keys()
 
-    pair_list = []
-    for p1 in pairs:
-        for p2 in keys:
-            p = f"{p1}_{p2}"
-            data2 = yf.download(p1, start=start_date, end=end_date, interval=p2)
+        pair_list = []
+        for p1 in pairs:
+            for p2 in keys:
+                p = f"{p1}_{p2}"
+                data2 = yf.download(p1, start=start_date, end=end_date, interval=p2)
 
-            # # Tickers list
-            # # We can add and delete any ticker from the list to get desired ticker live data
-            # ticker_list = ["DJIA", "DOW", "LB", "EXPE", "PXD", "MCHP", "CRM", "JEC", "NRG", "HFC", "NOW"]
-            # today = date.today()
+                # # Tickers list
+                # # We can add and delete any ticker from the list to get desired ticker live data
+                # ticker_list = ["DJIA", "DOW", "LB", "EXPE", "PXD", "MCHP", "CRM", "JEC", "NRG", "HFC", "NOW"]
+                # today = date.today()
 
-            df2 = pd.DataFrame(data2, columns=['DateTime', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
-            df2['DateTime'] = pd.to_datetime(df2['DateTime'], unit='m')
-            filename = ("data/{}_{}.csv").format(p1, p2)
-            df2.to_csv(filename)
-            #       plot2_("{}{}").format(p1,p2) = mpf.plot(data2,type='candle',mav=(20, 50, 200),volume=True,show_nontrading=True, warn_too_much_data=N)
+                df2 = pd.DataFrame(data2, columns=['DateTime', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
+                df2['DateTime'] = pd.to_datetime(df2['DateTime'], unit='m')
+                filename = ("data/{}_{}.csv").format(p1, p2)
+                df2.to_csv(filename)
+                #       plot2_("{}{}").format(p1,p2) = mpf.plot(data2,type='candle',mav=(20, 50, 200),volume=True,show_nontrading=True, warn_too_much_data=N)
 
-            if p not in pairs:
-                pair_list.append(p)
+                if p not in pairs:
+                    pair_list.append(p)
 
-    pair_list = pd.DataFrame(pair_list)
-    return (pair_list)
+        pair_list = pd.DataFrame(pair_list)
+        return (pair_list)
+
     pair_list = load_data()
-
     st.dataframe(pair_list)
+
 #====================================The End================================================================================
 
     # if Options == "Stocks":
