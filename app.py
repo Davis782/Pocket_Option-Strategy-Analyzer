27e850241ca327cfc862a8c6c46ebdbfbfb9f179
Davis782 <67481353+Davis782@users.noreply.github.com>
2021-11-07 12:21:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
+
 import streamlit as st
 import pandas as pd
 import base64
-#import matplotlib.pyplot as plt
-#import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
-#import yfinance as yf
+import yfinance as yf
 
-st.title('S&P 500 App')
+
+# import os, ssl
+#
+# if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+#         getattr(ssl, '_create_unverified_context', None)):
+#     ssl._create_default_https_context = ssl._create_unverified_context
+#
+# #  http://localhost:5997
+
+# Use the full page instead of a narrow central column
+st.set_page_config(layout="wide")
+
+st.title('Welcome to the Pocket Options Trading Analyzer')
 
 st.markdown("""
-This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+This app retrieves the Currency Pair data and runs it against numerous Strategies.  The results of passing the Strategies shows up in the tables below!
 * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
-* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
+* **Data source:** [Pocket Option](https://PocketOption.com).
+* **Suggestions:** Right Click above link and place in a new window, Open up a few Trading Currencies of interest and setup for Trading
 """)
 
 st.sidebar.header('User Input Features')
@@ -19,64 +33,49 @@ st.sidebar.header('User Input Features')
 # Web scraping of S&P 500 data
 #
 @st.cache
-def load_data():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    html = pd.read_html(url, header = 0)
-    df = html[0]
-    return df
+def load_data1():
+    url = '/Users/Owner/PycharmProjects/Streamlit_Projects/Forex_Streamlit_App/Practice_Code/Data_Science_Apps/df1.csv'
+    df1 = pd.read_csv(url, header = 0)
+    return df1
 
-df = load_data()
-sector = df.groupby('GICS Sector')
+def load_data2():
+    url = '/Users/Owner/PycharmProjects/Streamlit_Projects/Forex_Streamlit_App/Practice_Code/Data_Science_Apps/df2.csv'
+    df2 = pd.read_csv(url, header = 0)
+    return df2
+
+
+
+
+df1 = load_data1()
+df2 = load_data2()
+
+
+df1.reset_index()
+df2.reset_index()
+
+
+sector1 = df1.groupby('Market_Name')
+sector2 = df2.groupby('Market_Name')
+
 
 # Sidebar - Sector selection
-sorted_sector_unique = sorted( df['GICS Sector'].unique() )
-selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
+sorted_sector_unique1 = sorted( df1['Market_Name'].unique() )
+selected_sector1 = st.sidebar.multiselect('Market_Name', sorted_sector_unique1, sorted_sector_unique1, key = "test1")
+
+sorted_sector_unique2 = sorted( df2['Market_Name'].unique() )
+selected_sector2 = st.sidebar.multiselect('Market_Name', sorted_sector_unique2, sorted_sector_unique2, key = "test2")
 
 # Filtering data
-df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
+df_selected_sector1 = df1[ (df1['Market_Name'].isin(selected_sector1)) ]
+df_selected_sector2 = df2[ (df2['Market_Name'].isin(selected_sector2)) ]
 
-st.header('Display Companies in Selected Sector')
-st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
-st.dataframe(df_selected_sector)
 
-# Download S&P500 data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
-    return href
 
-st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
+st.header('Display Currencies in Selected Sector Going Down')
+st.write('Data Dimension: ' + str(df_selected_sector1.shape[0]) + ' rows and ' + str(df_selected_sector1.shape[1]) + ' columns.')
+st.dataframe(df_selected_sector1)
 
-# https://pypi.org/project/yfinance/
 
-data = yf.download(
-        tickers = list(df_selected_sector[:10].Symbol),
-        period = "ytd",
-        interval = "1d",
-        group_by = 'ticker',
-        auto_adjust = True,
-        prepost = True,
-        threads = True,
-        proxy = None
-    )
-
-# Plot Closing Price of Query Symbol
-def price_plot(symbol):
-  df = pd.DataFrame(data[symbol].Close)
-  df['Date'] = df.index
-  plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
-  plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
-  plt.xticks(rotation=90)
-  plt.title(symbol, fontweight='bold')
-  plt.xlabel('Date', fontweight='bold')
-  plt.ylabel('Closing Price', fontweight='bold')
-  return st.pyplot()
-
-num_company = st.sidebar.slider('Number of Companies', 1, 5)
-
-if st.button('Show Plots'):
-    st.header('Stock Closing Price')
-    for i in list(df_selected_sector.Symbol)[:num_company]:
-        price_plot(i)
+st.header('Display Currencies in Selected Sector Going Up')
+st.write('Data Dimension: ' + str(df_selected_sector2.shape[0]) + ' rows and ' + str(df_selected_sector2.shape[1]) + ' columns.')
+st.dataframe(df_selected_sector2)
