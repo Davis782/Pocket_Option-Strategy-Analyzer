86525ddcb0ef6717dba5b3807f919ca78249e868
Davis782 <67481353+Davis782@users.noreply.github.com>
2021-11-16 13:58:11 -0600

    Added - @st.cache(allow_output_mutation=True)

--- app.py
+++ app.py
@@ -48,5 +48,29 @@ else:
 		#Print columns A and E, which correspond to indices 0 and 4.
 		print('%s, %s'  (row[0], row[4]))
 
+@st.cache(allow_output_mutation=True)
+# @st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+
+# sheet_url= "https://docs.google.com/spreadsheets/d/1Ho6-7uRY3EuaPktr8a6uj9SshKyTaNNjI7uWg95nnfg/edit?usp=sharing"
+# st.write(sheet_url)
+
+sheet_url1 = st.secrets["private_gsheets_url"]
+d = sheet_url1
+st.write(d)
+# sheet_url1=d["private_gsheets_url"]
+st.write('private_gsheets_url' in d)
+# True
+
+# st.write(sheet_url1)
+# st.write(d["private_gsheets_url"])
+# sheet_url2=d["private_gsheets_url"]
+# st.write(sheet_url2)
+
+rows = run_query(f'SELECT * FROM "{d}"')
+
 
 
