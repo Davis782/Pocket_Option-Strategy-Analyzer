6423a22f7e6ab308a2e855f7c448f91eaacb8975
Davis782 <67481353+Davis782@users.noreply.github.com>
2021-11-18 16:52:48 -0600

    This is the new Autho Credent for info .toml

--- app.py
+++ app.py
@@ -1,79 +1,52 @@
-# Google Sheets - Python API, Read & Write Data  
+# Google Sheets - Python API, Read & Write Data
 # https://youtu.be/4ssigWmExak
+#https://developers.google.com/identity/protocols/oauth2/service-account#python
 #https://developers.google.com/sheets/api/reference/rest/v4/ValueInputOption
 #https://developers.google.com/sheets/api/quickstart/python
 
-#   pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+#   pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
 
 import streamlit as st
 import pandas as pd
-
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
+from gsheetsdb import connect # Create a connection object.
 
-import os.path
-from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
 
-SERVICE_ACCOUNT_FILE = 'keys.json'
 
-SCOPES = ['httgs://www.googleagis.com/auth/sgreadsheets']
+
+# Create a Google Authentication connection object
+# SERVICE_ACCOUNT_FILE = 'keys.json'
+# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 creds = None
-
-creds = service_account.Credentials.from_service_account_file(
-SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+creds =  service_account.Credentials.from_service_account_info( st.secrets["gcp_service_account"], scopes=[ "https://www.googleapis.com/auth/spreadsheets", ], )
+conn = connect(credentials=creds)
 
 # The ID spreadsheet.
-
-SAMPLE_SPREADSHEET_ID = '154DTvN5mNOTH_bexjYDZMZcSuprpX-gBZk_rnkm-8'
-
-service = build('sheets', 'v4', credentials=creds)
-
+SAMPLE_SPREADSHEET_ID = '1Ho6-7uRY3EuaPktr8a6uj9SshKyTaNNjI7uWg95nnfg'
+service = build('sheets','v4',credentials=creds)
+                
 # Call the Sheets API
 sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-range="sales!A1:G16").execute()
+                            range="Sheet1!A1:Z9999").execute()
 
 values = result.get('values', [])
+print(values, 'These are the values')
+
+
 aoa = [[ "1/1/2020",4000],["4/4/2020",3000],["7/12/2020",7000]]
-
 request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-range= "Sheet2!B2", valueInputOption="USER_ENTERED", body={ "values":aoa}).execute()
-print(request)
+                                range= "Sheet3!B2", valueInputOption="USER_ENTERED", body={"values":aoa}).execute()
+print(result, 'This is the result')
 
-if not values:
-	print('No data found.')
-else:
-	print('Name, Major:')
-	for row in values:
-		#Print columns A and E, which correspond to indices 0 and 4.
-		print('%s, %s'  (row[0], row[4]))
-
-@st.cache(allow_output_mutation=True)
-# @st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
-
-
-# sheet_url= "https://docs.google.com/spreadsheets/d/1Ho6-7uRY3EuaPktr8a6uj9SshKyTaNNjI7uWg95nnfg/edit?usp=sharing"
-# st.write(sheet_url)
-
-sheet_url1 = st.secrets["private_gsheets_url"]
-d = sheet_url1
-st.write(d)
-# sheet_url1=d["private_gsheets_url"]
-st.write('private_gsheets_url' in d)
-# True
-
-# st.write(sheet_url1)
-# st.write(d["private_gsheets_url"])
-# sheet_url2=d["private_gsheets_url"]
-# st.write(sheet_url2)
-
-rows = run_query(f'SELECT * FROM "{d}"')
-
-
-
+#
+# if not values:
+# 	print('No data found.')
+# else:
+# 	print('Name, Major:')
+# 	for row in values:
+# 		#Print columns A and E, which correspond to indices 0 and 4.
+# 		print('%s, %s',  (row[0], row[4]))
