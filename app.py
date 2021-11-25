6c049b486003c03b775fc11eb54f12b7d9a58cbf
Davis782 <67481353+Davis782@users.noreply.github.com>
2021-11-25 12:59:29 -0600

    creds

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ import streamlit as st
 import pandas as pd
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-
-
+import gspread
 
 
 # Create a Google Authentication connection object
 # SERVICE_ACCOUNT_FILE = 'keys.json'
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
 
 
 creds = None
@@ -33,19 +33,19 @@ service = build('sheets','v4',credentials=creds)
 # Call the Sheets API
 sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                            range="Sheet1!A1:Z9999").execute()
+                            range="Sheet3!A1:O22").execute()
 
 values = result.get('values', [])
 print(values, 'These are the values')
 st.write(values)
 
 
-# aoa = values 
-# request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#                                 range= "Sheet4!B2", valueInputOption="USER_ENTERED", body={"values":aoa}).execute()
-# st.write(request)
+aoa = values 
+request = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range= "Sheet4!B2", valueInputOption="USER_ENTERED", body={"values":aoa}).execute()
+st.write(request)
 
-df1 = pd.DataFrame.from_records(values)
+df1 = pd.DataFrame.from_records(request)
 st.write(df1)
 
 
